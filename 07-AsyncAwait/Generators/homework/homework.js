3ef50063ddcc896fbe4a18fa49a30102dccd0a09
Sojo506 <fabiansojowork@gmail.com>
2022-11-08 22:26:16 -0600

    Septima Homework Done

--- 07-AsyncAwait/Generators/homework/homework.js
+++ 07-AsyncAwait/Generators/homework/homework.js
@@ -1,6 +1,20 @@
-function fizzBuzzGenerator(max) {
+function* fizzBuzzGenerator(max) {
   // Tu código acá:
+  let number = 1;
+  
+  while(max ? number <= max: true) {
+    if(number%3 === 0 && number%5 === 0) {
+      yield 'Fizz Buzz';
+    }else if(number%5 === 0) {
+      yield 'Buzz'
+    }else if(number%3 ===0) {
+      yield 'Fizz';
+    }else {
+      yield number;
+    }
 
+    number++
+  }
 }
 
 module.exports = fizzBuzzGenerator;
